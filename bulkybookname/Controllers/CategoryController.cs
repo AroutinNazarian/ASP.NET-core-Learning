629dc309f88f770b2811cfeff741f029ed8ab305
AroutinNazarian <naroutin@gmail.com>
2022-01-30 14:50:12 +0330

    Add project files.

---
+++ bulkybookname/Controllers/CategoryController.cs
@@ -0,0 +1,40 @@
+﻿using bulkybookname.Data;
+using bulkybookname.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace bulkybookname.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDBcontext _db;
+
+        public CategoryController(ApplicationDBcontext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList = _db.categories;
+            return View(objCategoryList);
+        }
+        //GET
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+    }
+}
---
+++ bulkybookname/Data/ApplicationDBcontext.cs
@@ -0,0 +1,14 @@
+﻿using bulkybookname.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace bulkybookname.Data;
+
+public class ApplicationDBcontext : DbContext
+{
+      public ApplicationDBcontext(DbContextOptions<ApplicationDBcontext> options) : base(options)
+      {      
+      }
+        
+    public DbSet<Category> categories { get; set; }
+}
+
